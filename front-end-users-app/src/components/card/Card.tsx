0dbcd3693964a385ab6589c665a9595c23436f82
Wasim Abuzaher <wasim.abuzaher@gmail.com>
2022-02-24 00:21:58 -0500

    Add initial Card component

---
+++ front-end-users-app/src/components/card/Card.tsx
@@ -0,0 +1,8 @@
+import React, {FC, ReactElement} from "react";
+import classes from './Card.module.css'
+
+export const Card: FC = ({children}): ReactElement => {
+    return <div className={classes.card}>
+        {children}
+    </div>
+}
